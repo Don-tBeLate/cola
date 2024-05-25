907cc7d08b2299b17da1a8d3e5b42f95de69b28e
오진규 <ohjk5604@naver.com>
2024-05-25 15:30:05 +0900

    CORS

--- backend/domain/wav/wav.py
+++ backend/domain/wav/wav.py
@@ -72,6 +72,7 @@ async def wav_result_complicated():
     for f in file_list:
         name = f"{f[:-5]}.wav"
         path = os.path.join(UPLOAD_WAV_DIR, name)
+        return {"result": prob_list}
 
         sound = AudioSegment.from_file(f"./uploaded_mp3_files/{f}", 'webm')
         sound.export(f"./uploaded_wav_files/{name}", format="wav")
